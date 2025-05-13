425b099e8cfe389372bccbf62bbd16a2c4fe645f
YoloYao <1015205448@qq.com>
2025-05-13 01:44:59 +0100

    Add articles

--- docs/data/generated/available_img.js
+++ docs/data/generated/available_img.js
@@ -1,1 +1,11 @@
-const availableImg = ["Beck2016Visual"];
+const availableImg = [
+    "Kabir2018Uncertainty",
+    "Hüllermeier2021Uncertainty",
+    "Weytjens2022Uncertainty",
+    "Dai2024Kernel",
+    "Pawlowski2017Neural",
+    "Wu2024Classical",
+    "Ghoshal2022Uncertainty",
+    "Russell2021DeepLearning",
+    "Michelmore2018DNN",
+    "Chung2021MachineLearning"];
