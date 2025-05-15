e47adc28abbfd3d8b4eeb8548df2e47c21e674ab
YoloYao <1015205448@qq.com>
2025-05-15 11:19:42 +0100

    Update article type

--- docs/data/generated/available_img.js
+++ docs/data/generated/available_img.js
@@ -1,1 +1,1 @@
-const availableImg = ["Russell2021DeepLearning","Beck2016Visual","Weytjens2022Uncertainty","Dai2024Kernel","Michelmore2018DNN","Chung2021MachineLearning","Hüllermeier2021Uncertainty","Ghoshal2022Uncertainty","Pawlowski2017Neural","Kabir2018Uncertainty","Wu2024Classical"];
+const availableImg = ["Russell2021DeepLearning","Beck2016Visual","Weytjens2022Uncertainty","Dai2024Kernel","Michelmore2018DNN","Chung2021MachineLearning","H\u00fcllermeier2021Uncertainty","Ghoshal2022Uncertainty","Pawlowski2017Neural","Kabir2018Uncertainty","Wu2024Classical"];
